82b5c99bffaf315ed0f711b41adfce763e201d5a
belindaHBTN <6372@holbertonstudents.com>
2023-06-06 05:24:29 -0700

    changed to use enumerate method

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
 
-    for i in range(len(new_list)):
-        if new_list[i] == search:
+    for i, v in enumerate(new_list):
+        if v == search:
             new_list[i] = replace
     return new_list

a551104a58eadc70c6d29c03138cc0337caed184
belindaHBTN <6372@holbertonstudents.com>
2023-06-05 18:47:13 -0700

    updated the format

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,9 +2,8 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_zero = (0, 0)
-    new_tuple_a = tuple_a + tuple_zero # (1, 0, 0) (0, 0)
-    new_tuple_b = tuple_b + tuple_zero
+    n_tuple_a = tuple_a + tuple_zero
+    n_tuple_b = tuple_b + tuple_zero
 
-    new_tuple =((new_tuple_a[0] + new_tuple_b[0]), (new_tuple_a[1] +
-        new_tuple_b[1]))
-    return new_tuple
+    n_tuple = ((n_tuple_a[0] + n_tuple_b[0]), (n_tuple_a[1] + n_tuple_b[1]))
+    return n_tuple

4540eb00eaf2c7c211bd65271b669341730d8f8c
belindaHBTN <6372@holbertonstudents.com>
2023-06-06 06:53:39 -0700

    added condition

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list.copy()
+    if my_list:
+        new_list = my_list.copy()
     return list(map((lambda x: x * number), new_list))

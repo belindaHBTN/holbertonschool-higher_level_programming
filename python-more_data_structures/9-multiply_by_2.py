0ef92029d4400a4e7f2b72ae41b090f3e48ea64a
belindaHBTN <6372@holbertonstudents.com>
2023-06-07 03:03:27 -0700

    used dictionary comprehension instead of for loop

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    dictionary_copy = a_dictionary.copy()
-
-    for key in dictionary_copy:
-        dictionary_copy[key] = dictionary_copy[key] * 2
-    return dictionary_copy
+    new_dict = {k: v * 2 for (k, v) in a_dictionary.items()}
+    return new_dict

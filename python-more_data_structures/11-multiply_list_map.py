b4d25e7a270368713c6b93558d5c5d6c7d1720c9
belindaHBTN <6372@holbertonstudents.com>
2023-06-07 03:26:52 -0700

    fixed bug

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    if my_list:
-        new_list = my_list.copy()
-    return list(map((lambda x: x * number), new_list))
+    return list(map(lambda x: x * number, my_list))

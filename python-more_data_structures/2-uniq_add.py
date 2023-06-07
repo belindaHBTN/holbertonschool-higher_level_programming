532d769e0ba0a7fcaaa9517f14788fdff22b4e6e
belindaHBTN <6372@holbertonstudents.com>
2023-06-06 23:52:03 -0700

    used sum function instead of looping

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    new_list = list(set(my_list))
-
-    sum = 0
-    for i in new_list:
-        sum = sum + i
-    return sum
+    return sum(list(set(my_list)), 0)

50b3b2dd0b08f20deee5bc4220057768cc251037
belindaHBTN <6372@holbertonstudents.com>
2023-06-07 20:59:32 -0700

    updated format

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    roman_dict = {"M": 1000, "C": 100, "D": 500, "X": 10, "L": 50, "I": 1, "V": 5}
+    r_dict = {
+            "M": 1000, "C": 100, "D": 500, "X": 10, "L": 50, "I": 1, "V": 5
+            }
 
     if type(roman_string) is not str or roman_string is None:
         return 0
@@ -9,10 +11,10 @@ def roman_to_int(roman_string):
     num = 0
     for i in range(len(roman_string)):
         if i == len(roman_string) - 1:
-            num = num + roman_dict.get(roman_string[i])
-        elif roman_dict.get(roman_string[i]) < roman_dict.get(roman_string[i + 1]):
-            num = num - roman_dict.get(roman_string[i])
+            num = num + r_dict.get(roman_string[i])
+        elif r_dict.get(roman_string[i]) < r_dict.get(roman_string[i + 1]):
+            num = num - r_dict.get(roman_string[i])
         else:
-            num = num + roman_dict.get(roman_string[i])
+            num = num + r_dict.get(roman_string[i])
 
     return num

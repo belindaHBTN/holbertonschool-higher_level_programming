810bc4c1eb29afda76ce1b4f277e30e777a22c16
belindaHBTN <6372@holbertonstudents.com>
2023-06-08 05:40:26 -0700

    added conditions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,6 +3,9 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
+        if not my_list or not x:
+            print("")
+            return i
         for index in range(0, x):
             i = index
             print("{}".format(my_list[index]), end="")

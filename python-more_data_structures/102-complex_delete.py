25d5d46e764507def87419d69a64e94dae0705fb
belindaHBTN <6372@holbertonstudents.com>
2023-06-10 23:59:37 -0700

    --amend

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -4,7 +4,7 @@ def complex_delete(a_dictionary, value):
     keys = []
     for k, v in a_dictionary.items():
         if v == value:
-            keys.append[k]
+            keys.append(k)
     for k in keys:
         del a_dictionary[k]
 

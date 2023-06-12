3d6488c14aab0bd307195b60c68762cd0d773bf2
belindaHBTN <6372@holbertonstudents.com>
2023-06-12 04:57:26 -0700

    Write a class Square that defines a square by a private instance attribute size

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represents a square, with it's size"""
+
+    def __init__(self, size):
+        """Initializes the data"""
+        self.__size = size

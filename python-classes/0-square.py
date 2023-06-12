a28f62fb8cdd1280787f72111905f11446871499
belindaHBTN <6372@holbertonstudents.com>
2023-06-12 02:43:59 -0700

    updated document

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 
-"""Define a class Square"""
+"""Define a class Square."""
 
 
 class Square:
-    """empty class"""
+    """An empty class, only storing pass in it"""
     pass

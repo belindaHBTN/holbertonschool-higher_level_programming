f9344a4f62af49e23ed2fd9abd9276d74ec91c80
belindaHBTN <6372@holbertonstudents.com>
2023-06-13 05:19:28 -0700

    square class with getter and setter methods

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" Define a Square class. """
+
+
+class Square:
+    """ Represent a square """
+
+    def __init__(self, size=0):
+        """ Initializes a Square object with a given size.
+        Args:
+            size (int): The size of the square (default is 0).
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Size Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ Size Setter"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Return area of square"""
+        return self.__size ** 2

9216d61d1812cc43c4623fb83730f216f743b06e
belindaHBTN <6372@holbertonstudents.com>
2023-06-17 21:59:51 -0700

    added one condition

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -66,4 +66,7 @@ class Rectangle:
         """
             Method that calculate the perimeter of the rectangle
         """
+        if self.width == 0 or self.height == 0:
+            return 0
+
         return self.width * 2 + self.height * 2

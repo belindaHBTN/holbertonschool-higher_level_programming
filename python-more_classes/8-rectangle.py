5ed470ee3b9a4a02ae1b5629d052b5bc39a0b30c
belindaHBTN <6372@holbertonstudents.com>
2023-06-17 23:11:32 -0700

    updated comparation

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -114,9 +114,7 @@ class Rectangle:
         if not isinstance(rect_2, Ractangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_1.area() > rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
-        if rect_1.area() == rect_2.area():
-            return rect_1

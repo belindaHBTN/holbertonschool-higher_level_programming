cada287a1ea4d254387c0111ec73a0198165da80
belindaHBTN <6372@holbertonstudents.com>
2023-06-19 04:22:11 -0700

    added a documentation for a function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def lookup(obj):
+    """Function that returns the list of available attributes and methods.
+
+    Args:
+        obj: the instance of the class
+
+    Returns:
+        The list of attributes and methods of an obj
+    """
     return dir(obj)

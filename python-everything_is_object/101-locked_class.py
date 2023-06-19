1173ad50483b992dbfb646b1644a10b3de971fce
belindaHBTN <6372@holbertonstudents.com>
2023-06-19 00:14:58 -0700

    change to no attribute init

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -11,5 +11,8 @@ class LockedClass:
     """
     __slots__ = ['first_name']
 
-    def __init__(self, first_name):
-        self.first_name = first_name
+    def __init__(self):
+        """
+            Method that initializes the instance
+        """
+        ...

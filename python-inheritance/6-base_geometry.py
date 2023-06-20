8a062e388931e8ad6e97666eff4c33c21f4e01f8
belindaHBTN <6372@holbertonstudents.com>
2023-06-19 23:39:31 -0700

    added a public intance method to BaseGeometry class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+    This module provides a BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+        This is a class with Geometry
+    """
+    def area(self):
+        """
+            Public instance method to raise an exception
+        """
+        raise Exception("area() is not implemented")

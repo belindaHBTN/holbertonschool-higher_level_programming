f884c0951d9a3c086e5d5d6e4a96c77ed38dfd30
belindaHBTN <6372@holbertonstudents.com>
2023-06-20 16:00:16 -0700

    added a rectangle subclass

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""
+    This module provides a BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+        This is a class with Geometry
+    """
+    def area(self):
+        """
+            Public instance method to raise an exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+            Public instance method to validates value
+
+            Args:
+            name: always a string
+            value: the value need to be validated
+
+            Errors:
+            TypeError: value is not an integer
+            ValueError: value is equal or less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+        This is a subclass of BaseGeometry, represents a rectangle
+    """
+    def __int__(self, width, height):
+        """
+            Method that initializes the instance
+
+            Args:
+                width: width of the rectangle
+                height: height of the rectangle
+        """
+        self.integer_validator(width)
+        self.integer_validator(height)
+        self.__width = width
+        self.__height = height

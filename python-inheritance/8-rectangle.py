3be352592a4ef0cfeb8a745132a6a292f6571dfd
belindaHBTN <6372@holbertonstudents.com>
2023-06-22 21:15:09 -0700

    used import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,33 +4,7 @@
     This module provides a BaseGeometry class.
 """
 
-
-class BaseGeometry:
-    """
-        This is a class with Geometry
-    """
-    def area(self):
-        """
-            Public instance method to raise an exception
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-            Public instance method to validates value
-
-            Args:
-            name: always a string
-            value: the value need to be validated
-
-            Errors:
-            TypeError: value is not an integer
-            ValueError: value is equal or less than 0
-        """
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

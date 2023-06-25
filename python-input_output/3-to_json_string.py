66a5cd4f3d2c0cabc124c1eb22c8d1cf3b070fd1
belindaHBTN <6372@holbertonstudents.com>
2023-06-24 21:12:04 -0700

    updated format

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,14 +2,16 @@
 """
     This module contains a function that convert obj to JSON format
 """
-
-
 import json
+
 
 def to_json_string(my_obj):
     """ Function that convert obj to JSON format
 
     Args:
         my_obj: the obj that need to be converted
+
+    Returns:
+        JSON format object
     """
     return json.dumps(my_obj)

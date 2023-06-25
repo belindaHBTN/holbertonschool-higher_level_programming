b836a09e52e03892d9f5df28439cfb9bdeb6632c
belindaHBTN <6372@holbertonstudents.com>
2023-06-24 19:25:22 -0700

    added mode and end=''

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,11 +3,12 @@
     This module contains a function that read text from a file
 """
 
+
 def read_file(filename=""):
     """ Function tha tread text from a file
 
     Args:
         filename: optional arg, filename
     """
-    with open(filename, encoding="utf-8") as f:
-        print(f.read())
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")

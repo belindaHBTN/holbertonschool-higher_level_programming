9cac04651275926ea338eaa44460a2ad5bbae241
belindaHBTN <6372@holbertonstudents.com>
2023-06-25 01:31:42 -0700

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+    This module contains a Student class
+"""
+
+
+class Student:
+    """
+        A class that represent student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            initialise an instance of student Class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Function that retrieves a dictionary representation of an instance
+        Args:
+            attrs: if it is a list of strings or not
+
+        Return:
+            a dictionary
+        """
+        student_dict = self.__dict__
+
+        if type(attrs) is not list:
+            return student_dict
+        else:
+            dict = {}
+            for attr in attrs:
+                for el in student_dict:
+                    if el == attr:
+                        dict[el] = student_dict[el]
+            return dict
+
+    def reload_from_json(self, json):
+        """ Function that replaces all attributes of the Student instance
+        Args:
+            json: json string
+        """
+        for key in json:
+            self.__dict__[key] = json[key]

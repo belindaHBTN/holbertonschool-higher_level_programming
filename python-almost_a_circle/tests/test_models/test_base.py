4b2fda5e4c3ca0434368c3919f3174368aafe101
belindaHBTN <6372@holbertonstudents.com>
2023-06-27 05:20:23 -0700

    added first test

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Module for testing Base class"""
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """test class"""
+    def test_no_id(self):
+        """test default id, no prameter """
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+    def test_with_id(self):
+        """test id with value been pasted in"""
+        b2 = Base(12)
+        self.assertEqual(b2.id, 12)
+
+    def test_increment_id(self):
+        """test id increment"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+        b4 = Base()
+        self.assertEqual(b4.id, 3)

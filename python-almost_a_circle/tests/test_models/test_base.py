2dc4dac0caa82a28e3ab7f42d6ec57d21296e36d
belindaHBTN <6372@holbertonstudents.com>
2023-06-28 00:15:28 -0700

    concluded tests into one test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,12 +7,14 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     """test class"""
-    def test_no_id(self):
-        """test default id, no prameter """
+
+    def test_id(self):
+        """test id"""
         b1 = Base()
         self.assertEqual(b1.id, 1)
-
-    def test_with_id(self):
-        """test id with value been pasted in"""
-        b1 = Base(12)
-        self.assertEqual(b1.id, 12)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+        b4 = Base()
+        self.assertEqual(b4.id, 3)

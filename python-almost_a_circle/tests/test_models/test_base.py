6084c651de462e0684763a734beae30e91e71bb4
belindaHBTN <6372@holbertonstudents.com>
2023-06-27 23:30:43 -0700

    delete one test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -16,12 +16,3 @@ class TestBase(unittest.TestCase):
         """test id with value been pasted in"""
         b1 = Base(12)
         self.assertEqual(b1.id, 12)
-
-    def test_increment_id(self):
-        """test id increment"""
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-        b2 = Base(12)
-        self.assertEqual(b2.id, 12)
-        b3 = Base()
-        self.assertEqual(b3.id, 2)

26d2befd0cb591904fc21f9927f2bf9258fea5bf
belindaHBTN <6372@holbertonstudents.com>
2023-06-29 00:48:46 -0700

    deleted exception error message

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -34,9 +34,5 @@ class Square(Rectangle):
         """
             size setter
         """
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value <= 0:
-            raise ValueError("size must be > 0")
         self.width = value
         self.height = value

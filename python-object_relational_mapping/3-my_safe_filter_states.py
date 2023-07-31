a667d0e356607c4b7ddd95a9b0c05c11d4bd548f
belindaHBTN <6372@holbertonstudents.com>
2023-07-31 03:05:28 -0700

    3. SQL Injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Lists all values in the states table where name match arg"""
+
+import MySQLdb
+from sys import argv
+
+
+def get_state(username, password, db_name, search_name):
+    """Lists all values in the states table where name match arg"""
+    db = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            port=3306,
+            db=db_name
+    )
+    cursor = db.cursor()
+
+    cursor.execute(
+            "SELECT id, name FROM states\
+                    WHERE name LIKE BINARY %(search_name)s\
+                    ORDER BY id;",{'search_name': search_name}
+    )
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    get_state(argv[1], argv[2], argv[3], argv[4])

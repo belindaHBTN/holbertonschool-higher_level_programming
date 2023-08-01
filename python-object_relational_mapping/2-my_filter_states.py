6ad9413a369d54b4331773d22a1e0669051a6526
belindaHBTN <6372@holbertonstudents.com>
2023-07-31 23:43:25 -0700

    9. Contains

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Lists all values in the states table where name match arg"""
+"""Display all values in states table that matche the search name"""
 
 import MySQLdb
 from sys import argv
 
 
 def get_state(username, password, db_name, search_name):
-    """Lists all values in the states table where name match arg"""
+    """Display all values in states table that matche the search name"""
     db = MySQLdb.connect(
             host='localhost',
             user=username,
---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """Start link class to table in database"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    """Create a session"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """Querying"""
+    states = session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))

eed96f0ec4f0ac047c1725623519f2cc0ed32396
belindaHBTN <6372@holbertonstudents.com>
2023-07-31 20:31:59 -0700

    added string format

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,8 +10,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """Start link class to table in database"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'\
-            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     """Create a session"""
@@ -20,4 +20,4 @@ if __name__ == "__main__":
 
     """Querying"""
     for instance in session.query(State).order_by(State.id):
-        print(instance.id, ':', instance.name)
+        print("{}: {}".format(instance.id, instance.name))

00857d70cc34845771b5669578924918d49c9e29
belindaHBTN <6372@holbertonstudents.com>
2023-09-19 01:12:20 +0000

    added situation for invalid input

--- javascript-web_jquery/102-script.js
+++ javascript-web_jquery/102-script.js
@@ -1,14 +1,20 @@
+const langs = ['ar', 'az', 'be', 'bg', 'bn', 'bs', 'cs', 'da', 'de', 'dz', 'el', 'en', 'en - gb', 'en - us', 'es', 'et', 'fa', 'fi', 'fil', 'fr', 'he', 'hi', 'hr', 'hu', 'hy', 'id', 'is', 'it', 'ja', 'ka', 'kk', 'km', 'ko', 'lb', 'lo', 'lt', 'lv', 'mk', 'mn', 'ms', 'my', 'ne', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv', 'sw', 'th', 'tk', 'uk', 'vi', 'zh'];
+
 $(function() {
-  $("#btn_translate").click(function() {
-    let input = $("#language_code").val();
+  $('#btn_translate').click(function () {
+    const input = $('#language_code').val();
     // console.log(input);
-    $.ajax({
-      type: "GET",
-      url: `https://hellosalut.stefanbohacek.dev/?lang=${input}`,
-      success: function(data) {
-        console.log(data);
-        $("#hello").text(data.hello);
-      }
-    })
-  })
-})
+    if (langs.includes(input)) {
+      $.ajax({
+        type: 'GET',
+        url: `https://hellosalut.stefanbohacek.dev/?lang=${input}`,
+        success: function (data) {
+          console.log(data);
+          $('#hello').text(data.hello);
+        }
+      });
+    } else {
+      $('#hello').text('Please enter a valid lang code.');
+    };
+  });
+});
